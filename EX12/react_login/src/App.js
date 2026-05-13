d8f078d37098d0dab0bcd5224861348192c48776
raccoon0329 <kevinkuma33@gmail.com>
2026-05-13 14:05:06 +0800

    Add files via upload

---
+++ EX12/react_login/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignInSide from './SignInSide';
+import HelloCGU from './cgu_hello';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <SignInSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ EX12/react_login/src/cgu_hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloCGU = () => {
+  return (
+    <h1>hello CGU!!</h1>
+  );
+}
+
+export default HelloCGU;
